dea0148ed2b11b1d51499a40e57c7f8cee846d42
KaiZenSec <13855261+KaiZenSec@users.noreply.github.com>
2019-04-03 15:07:23 -0400

    Update campaign.sh

--- campaign.sh
+++ campaign.sh
@@ -4,7 +4,7 @@
 # 3 = password
 if [ "$1" != "" ] && [ "$2" != "" ]&& [ "$3" != "" ]; then
 
-echo INSERT INTO domains (domain) VALUES (\'$1\');
+echo INSERT INTO domains (domain) VALUES (''$1'');
 echo INSERT INTO users (id,name,maildir,crypt) VALUES ('$2@$1','short description','$2/',encrypt('$3', CONCAT('$5$', MD5(RAND()))) );
 echo INSERT INTO aliases (mail,destination) VALUES('$2@$1','$2@$1');
 
