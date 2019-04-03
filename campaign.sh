847af8f41edbc8047328bc047e36493a761b163d
KaiZenSec <13855261+KaiZenSec@users.noreply.github.com>
2019-04-03 14:53:30 -0400

    Update campaign.sh

--- campaign.sh
+++ campaign.sh
@@ -4,10 +4,10 @@
 # 3 = password
 if [ "$1" != "" ] && [ "$2" != "" ]&& [ "$3" != "" ]; then
 
-mysql -D maildb -e "INSERT INTO domains (domain) VALUES ('$1');"
-mysql -D maildb -e "INSERT INTO aliases (mail,destination) VALUES ('@$1','$2@$1');"
-mysql -D maildb -e "INSERT INTO users (id,name,maildir,crypt) VALUES ('$2@$1','$2','$2/',encrypt('$3', CONCAT('$5$', MD5(RAND()))) );"
-#mysql -D maildb -e "INSERT INTO relays (recipient,status) values ('@$1','OK');"
+echo INSERT INTO domains (domain) VALUES ('$1');
+echo INSERT INTO aliases (mail,destination) VALUES ('@$1','$2@$1'),('postmaster@domain.tld','$2@$1'),('$2@$1','$2@$1');	
+echo INSERT INTO users (id,name,maildir,crypt) VALUES ('$2@$1','short description','foldername/',encrypt('$1', CONCAT('$5$', MD5(RAND()))) );
+echo INSERT INTO aliases (mail,destination) VALUES('$2@$1','$2@$1');"
 
 else
     echo "Enter campaign domain and username ex. campaign.sh domain username password"
