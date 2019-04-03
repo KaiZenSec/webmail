cb147ca75859da0b5ea09241792a2ce283923523
KaiZenSec <13855261+KaiZenSec@users.noreply.github.com>
2019-04-03 14:48:05 -0400

    Update campaign.sh

--- campaign.sh
+++ campaign.sh
@@ -5,9 +5,9 @@
 if [ "$1" != "" ] && [ "$2" != "" ]&& [ "$3" != "" ]; then
 
 mysql -D maildb -e "INSERT INTO domains (domain) VALUES ('$1');"
-#mysql -D maildb -e "INSERT INTO aliases (mail,destination) VALUES ('@$1','$2@$1');"
+mysql -D maildb -e "INSERT INTO aliases (mail,destination) VALUES ('@$1','$2@$1');"
 mysql -D maildb -e "INSERT INTO users (id,name,maildir,crypt) VALUES ('$2@$1','$2','$2/',encrypt('$3', CONCAT('$5$', MD5(RAND()))) );"
-mysql -D maildb -e "INSERT INTO relays (recipient,status) values ('@$1','OK');"
+#mysql -D maildb -e "INSERT INTO relays (recipient,status) values ('@$1','OK');"
 
 else
     echo "Enter campaign domain and username ex. campaign.sh domain username password"
